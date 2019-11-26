4daa9d9f7aebd540dd1fde649bd21665ced4fe61
Alastair Gilles <agilles91@gmail.com>
2019-11-26 16:54:13 +0100

    Refactored routes

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params["Player 1"]
-    @player_2 = params["Player 2"]
-    erb :play
+    session['Player 1'] = params["Player 1"]
+    session['Player 2'] = params["Player 2"]
+    redirect to '/play'
   end
 
+  get '/play' do
+    @player_1 = session['Player 1']
+    @player_2 = session['Player 2']
+    erb :play
+  end
 end
